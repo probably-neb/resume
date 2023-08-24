4eb3a99cfec287633a98bd6bb08537a191b30508
Ben Kunkle <ben.kunkle@gmail.com>
2023-08-24 14:58:17 -0700

    use react-pdf instead of puppeteer

--- resume.tsx
+++ resume.tsx
@@ -1,35 +1,45 @@
 import React, { FC } from "react";
 import { renderToString } from "react-dom/server";
-import puppeteer from "puppeteer";
+import ReactPDF, { Document, Page, Text, View } from "@react-pdf/renderer";
 import fs from "fs";
 import toml from "toml";
-import {z} from "zod";
+import { z } from "zod";
+import { createTw } from "react-pdf-tailwind";
+
+const tw = createTw({
+    theme: {
+        fontFamily: {},
+        extend: {
+            colors: {},
+        },
+    },
+});
 
 const ContactInfoSchema = z.object({
     val: z.string(),
     icon: z.string(),
     pretty: z.string().optional(),
     href: z.boolean().optional(),
-})
-type ContactInfo = z.infer<typeof ContactInfoSchema>
+});
+type ContactInfo = z.infer<typeof ContactInfoSchema>;
 
 const ProjectSchema = z.object({
     name: z.string(),
     short: z.string(),
     exclude: z.boolean().optional(),
     skills: z.array(z.string()).optional(),
-    dates: z.string(),
-    steps: z.array(z.string()),
+    dates: z.string().optional(),
+    steps: z.array(z.string()).optional(),
     type: z.string().optional(),
-})
-type Project = z.infer<typeof ProjectSchema>
+});
+type Project = z.infer<typeof ProjectSchema>;
 
 const EducationSchema = z.object({
     notable_completed: z.array(z.string()),
     completed: z.array(z.string()),
     current: z.array(z.string()),
-})
-type Education = z.infer<typeof EducationSchema>
+});
+type Education = z.infer<typeof EducationSchema>;
 
 const ConfigSchema = z.object({
     languages: z.record(z.string(), z.string()),
@@ -37,52 +47,59 @@ const ConfigSchema = z.object({
     header_info: z.array(ContactInfoSchema),
     projects: z.array(ProjectSchema),
     calpoly: EducationSchema,
-})
-type Config = z.infer<typeof ConfigSchema>
+});
+type Config = z.infer<typeof ConfigSchema>;
 
 interface DividerProps {}
 const Divider: FC<DividerProps> = () => {
     return <div className="w-full border-b-2 border-gray-100 my-4"></div>;
 };
 
+interface ContactInfoProps {
+    info: ContactInfo;
+}
+
+const ContactInfo: FC<ContactInfoProps> = ({ info }) => {
+    return <Text>{info.pretty ?? info.val}</Text>;
+};
+
+interface ContactInfoListProps {
+    infos: ContactInfo[];
+}
+
+const ContactInfoList: FC<ContactInfoListProps> = ({ infos }) => {
+    return (
+        <View style={tw("flex flex-row")}>
+            {infos.map((info) => (
+                <ContactInfo info={info} key={info.val} />
+            ))}
+        </View>
+    );
+};
+
 interface ResumeProps {
     config: Config;
 }
-const Resume: FC<ResumeProps> = ({config}) => {
+
+const Resume: FC<ResumeProps> = ({ config }) => {
     return (
-        <html>
-            <head>
-                <script src="https://cdn.tailwindcss.com"></script>
-            </head>
-            <body style={{ width: "8.5in", height: "11in" }} className="">
-                <div className="flex justify-center">
-                    <p className="text-4xl">Ben Kunkle</p>
-                </div>
+        <Document>
+            <Page style={tw("m-[0.25in]")}>
+                <View style={tw("flex flex-col")}>
+                    <Text style={tw("text-4xl text-center")}>Ben Kunkle</Text>
+                    <ContactInfoList infos={config.header_info} />
+                </View>
                 <Divider />
-                {JSON.stringify(config)}
-            </body>
-        </html>
+            </Page>
+        </Document>
     );
 };
 
 const loadConfig = (): Config => {
     let raw = fs.readFileSync("info.toml");
-    const config = toml.parse(raw.toString('utf-8'))
+    const config = toml.parse(raw.toString("utf-8"));
     return ConfigSchema.parse(config);
-}
+};
 
-async function generatePDFPuppeteer(): Promise<Buffer> {
-    const resume = renderToString(<Resume config={config}/>);
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(resume);
-    const pdf = await page.pdf({ format: "letter" });
-    await browser.close();
-    return pdf;
-}
-
-generatePDFPuppeteer().then((pdf) => {
-    fs.writeFile("resume.pdf", pdf, (err) => {
-        if (err) console.error(err);
-    });
-});
+const config = loadConfig();
+ReactPDF.render(<Resume config={config} />, "resume.pdf");
