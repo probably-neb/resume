af1ace90e4f289994ae9d7d61a0f52eee3f61840
Ben Kunkle <ben.kunkle@gmail.com>
2023-08-28 09:23:38 -0700

    add width field to Divider

--- src/Divider.tsx
+++ src/Divider.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 
 interface DividerProps {
-    orientation?: "horizontal" | "vertical"
-    lineStyle?: "solid" | "dashed"
+    orientation?: "horizontal" | "vertical";
+    lineStyle?: "solid" | "dashed";
+    width?: number;
 }
 
-export default function Divider({orientation, lineStyle}: DividerProps) {
-    orientation = orientation ?? "horizontal";
-    lineStyle = lineStyle ?? "solid";
-    let styles = ["border-b-2", "border-gray-500"];
+export default function Divider(props: DividerProps) {
+    const defaults: DividerProps = {
+        orientation: "horizontal",
+        lineStyle: "solid",
+        width: 2,
+    };
+    const { orientation, lineStyle, width } = Object.assign(defaults, props);
+    let styles = ["border-gray-500"];
     if (orientation === "vertical") {
-        styles.push("h-full")
+        styles.push(`border-r-${width}`);
     } else {
-        styles.push("w-full")
+        styles.push("w-full");
+        styles.push(`border-b-${width}`);
     }
     if (lineStyle === "dashed") {
-        styles.push("border-dashed")
+        styles.push("border-dashed");
     }
-    return (
-        <div className={styles.join(" ")}></div>
-    );
-};
+    return <div className={styles.join(" ")}></div>;
+}
