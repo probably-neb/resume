a763360f41a95105b051768892a2d4bb684cc02c
Ben Kunkle <ben.kunkle@gmail.com>
2023-08-28 09:24:26 -0700

    better projects layout

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,14 +1,46 @@
-import React, { FC } from 'react';
-import { Project } from './config';
+import React, { FC } from "react";
+import { Project } from "./config";
+import { Buffer } from "./utils";
 
 const ProjectItem: FC<{ project: Project }> = ({ project }) => {
-    return <div>{project.name}</div>
-}
+    return (
+        <div>
+            <div className="flex flex-row">
+                <p>
+                    <b>{project.name}</b>
+                </p>
+                <Buffer width="4px" />
+                {!!project.type ? (
+                    <p>
+                        <i>{project.type}</i>
+                    </p>
+                ) : null}
+            </div>
+            <div className="text-sm">{project.short}</div>
+            {!!project.steps && (
+                <div className="flex flex-row">
+                    <Buffer width="40px" />
+                    <ul style={{ listStyleType: "disc" }}>
+                        {project.steps.map((step) => (
+                            <li key={step}>{step}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
 
 const ProjectList: FC<{ projects: Project[] }> = ({ projects }) => {
-    return <div>
-        {projects.map((project) => <ProjectItem project={project} key={project.name}/>)}
-    </div>
-}
+    return (
+        <div>
+            {projects
+                .filter((p) => !p.exclude)
+                .map((project) => (
+                    <ProjectItem project={project} key={project.name} />
+                ))}
+        </div>
+    );
+};
 
 export default ProjectList;
